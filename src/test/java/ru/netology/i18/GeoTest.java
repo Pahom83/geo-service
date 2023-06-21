dcb9384889bda29a179c0e0df003b6a6af6b1a80
Pahom83 <pol_83@mail.ru>
2023-06-21 19:56:08 +0400

    Tests Rework

--- src/test/java/ru/netology/i18/GeoTest.java
+++ src/test/java/ru/netology/i18/GeoTest.java
@@ -1,12 +1,9 @@
 package ru.netology.i18;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import ru.netology.entity.Country;
-import ru.netology.geo.GeoService;
-import ru.netology.geo.GeoServiceImpl;
-import ru.netology.i18n.LocalizationService;
-import ru.netology.i18n.LocalizationServiceImpl;
+import ru.netology.geo.*;
+import ru.netology.i18n.*;
 
 public class GeoTest {
 
@@ -14,30 +11,42 @@ public class GeoTest {
     GeoService geo = new GeoServiceImpl();
     @Test
 
-    void localizationServiceTest(){
+    void localizationServiceRusTest(){
         String expectedRus = "Добро пожаловать";
-        String expectedOther = "Welcome";
-        String testRus = test.locale(Country.RUSSIA);
-        String testUsa = test.locale(Country.USA);
-        String testGermany = test.locale(Country.GERMANY);
-        String testBrazil = test.locale(Country.BRAZIL);
+        String actualRus = test.locale(Country.RUSSIA);
+        String actualUsa = test.locale(Country.USA);
         //test
-        Assertions.assertEquals(expectedRus, testRus);
-        Assertions.assertEquals(expectedOther, testGermany);
-        Assertions.assertEquals(expectedOther, testUsa);
-        Assertions.assertEquals(expectedOther, testBrazil);
-        Assertions.assertNotEquals(expectedOther, testRus);
+        Assertions.assertEquals(expectedRus, actualRus);
+        Assertions.assertNotEquals(expectedRus, actualUsa);
     }
 
     @Test
-    void geoServiceImplTest(){
-        Country expectedRus= Country.RUSSIA;
-        Country expectedUsa = Country.USA;
+    void localizationServiceOtherTest() {
+        String expectedOther = "Welcome";
+        String actualUsa = test.locale(Country.USA);
+        String actualGermany = test.locale(Country.GERMANY);
+        String actualBrazil = test.locale(Country.BRAZIL);
+        //test
+        Assertions.assertEquals(expectedOther, actualGermany);
+        Assertions.assertEquals(expectedOther, actualUsa);
+        Assertions.assertEquals(expectedOther, actualBrazil);
+    }
+    @Test
+    void geoServiceImplRusTest(){
+        Country expectedRus = Country.RUSSIA;
 
-        Country actualUsa = geo.byIp("96.").getCountry();
         Country actualRus = geo.byIp("172.").getCountry();
 
         Assertions.assertEquals(expectedRus, actualRus);
+    }
+
+    @Test
+    void geoServiceImplUsaTest(){
+        Country expectedUsa = Country.USA;
+
+        Country actualUsa = geo.byIp("96.").getCountry();
+
         Assertions.assertEquals(expectedUsa, actualUsa);
     }
+
 }
